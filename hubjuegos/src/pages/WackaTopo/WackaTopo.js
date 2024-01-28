c8a0987e1c2255e97f34ed8ff9d5376d42c1a312
Xiao Xue <anxxue@gmail.com>
2024-01-28 21:07:17 +0100

    Styling buttons

--- hubjuegos/src/pages/WackaTopo/WackaTopo.js
+++ hubjuegos/src/pages/WackaTopo/WackaTopo.js
@@ -6,13 +6,15 @@ import "./WackaTopo.css";
 /* Ésto es el HTML template del DOM, es nuestra base. Se encuentran los elementos principales como "Ids,buttons,holes,score etc..."-
 En el contenedor "div" se establece el juego con todos los hoyos.*/
 const template = () => `
-    <h1>Let's hit all the moles!</h1>
+    
     <div class="game-info"> 
+        <h1>Let's hit all the moles!</h1>
         <div id="score">Score: 0</div> 
         <div id="timer">Time: 30s</div> 
+        <button id="startButton">Start Playing</button> 
+        <button id="endButton" disabled>End Playing</button> 
     </div> 
-    <button id="startButton">Start Playing</button> 
-    <button id="endButton" disabled>End Playing</button> 
+ 
     <div class="game-container">
         <div class="hole" id="hole1"></div> 
         <div class="hole" id="hole2"></div> 
